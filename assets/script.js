8012e365984a09bcd2d6027cc08d3af9988303df
kweinerman <85956994+kweinerman@users.noreply.github.com>
2021-08-07 11:36:23 -0600

    Added Random cocktail generator

--- assets/script.js
+++ assets/script.js
@@ -172,3 +172,61 @@ Email.send({
     console.log('Fetch Error :-S', err);
   });
  }
+
+ function getRandomCocktail(){
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+      
+        displayRandomCocktail(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+ }
+
+ getRandomCocktail();
+
+ function displayRandomCocktail(cocktail){
+   console.log(cocktail.drinks[0]);
+   let drinkSection = document.getElementById('random-cocktail');
+
+   let drinkName = document.createElement('h2');
+   
+   drinkName.innerHTML = cocktail.drinks[0].strDrink;
+
+   drinkSection.appendChild(drinkName);
+
+   let img = document.createElement('img')
+   img.src = cocktail.drinks[0].strDrinkThumb
+
+   drinkSection.appendChild(img);
+
+   for (let i=1; i<16; i++) {
+
+    if(cocktail.drinks[0][`strIngredient${i}`] == null || cocktail.drinks[0][`strIngredient${i}`] == '' ){
+      break;
+    }
+ 
+  
+  let ingredient = document.createElement('ons-list-item');
+  ingredient.innerHTML = cocktail.drinks[0][`strIngredient${i}`] + ':' + cocktail.drinks[0][`strMeasure${i}`]
+
+  drinkSection.appendChild(ingredient);
+   }
+
+   let card = document.createElement('ons-card');
+   card.innerHTML = cocktail.drinks[0].strInstructions;
+
+   drinkSection.appendChild(card);
+}
