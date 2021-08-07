3b759776ee1425535ae5e3ab345f33de034a14ac
kweinerman <85956994+kweinerman@users.noreply.github.com>
2021-08-07 10:02:21 -0600

    Updated script.js to match my version

--- assets/script.js
+++ assets/script.js
@@ -27,7 +27,7 @@ var marker = new google.maps.Marker({
 // })
 
 
-var marker = new google.maps.Marker({
+var marker1 = new google.maps.Marker({
   position:{lat:39.734572,lng:-104.9618063},
   map:map
   
@@ -37,15 +37,138 @@ var marker = new google.maps.Marker({
 //   content: '<h1>Fresh State Bev Co</h1>'
 // });
 
-var marker = new google.maps.Marker({
+var marker2 = new google.maps.Marker({
+  position:{lat:39.6964946,lng:-104.9496032},
+  map:map
+  
+});
+
+var marker3 = new google.maps.Marker({
   position:{lat:39.6964946,lng:-104.9496032},
   map:map
   
 });
 }
 
-var marker = new google.maps.Marker({
-  position:{lat:39.6964946,lng:-104.9496032},
-  map:map
-  
-});
+
+
+// Contact Form Mailer
+// Your web app's Firebase configuration
+//   For Firebase JS SDK v7.20.0 and later, measurementId is optional
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyCNwjSzzIbkbNncKLvY6OCxoPxxIHwhmgM",
+//     authDomain: "freshstate-85fbf.firebaseapp.com",
+//     projectId: "freshstate-85fbf",
+//     storageBucket: "freshstate-85fbf.appspot.com",
+//     messagingSenderId: "578118481416",
+//     appId: "1:578118481416:web:bcdc4164716e75064583bd",
+//     measurementId: "G-ZTCPWKX022"
+//   };
+//   // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
+//   firebase.analytics();
+
+// // reference contactInfo collection
+// let contactInfo = firebase.database().ref("infos")
+
+// Listen for a submit
+
+document.getElementById("form-submission").addEventListener("click", sendEmail);
+
+
+
+//run submit form function 
+function submitForm(event) {
+  event.preventDefault();
+  //get input values
+  let name = document.querySelectorAll(".name").value;
+  let email = document.querySelectorAll(".email").value;
+  let message = document.querySelectorAll(".message").value;
+
+  saveContactInfo(name, email, message);
+
+  document.querySelectorAll("form-submission").requestFullscreen();
+
+  sendEmail(name, email, message);
+}
+
+// function saveContactInfo(name, email, message) {
+//   let newContactInfo = contactInfo.push();
+
+//   newContactInfo.set({
+//     name: name,
+//     email: email,
+//     message:message
+//   });
+
+//   retreiveInfo()
+// }
+
+// Retrive Info
+// function retrieveInfo(){
+//   let ref = firebase.database().ref("info");
+//   ref.on("value", gotData);
+// }
+
+// function gotData(data){
+//   let info =data.val();
+//   let keys = Object.keys(info);
+
+//   for (let i = 0; i < keys.length; i++) {
+//     let infoData = keys[i];
+//     let name = info[infoData].name;
+//     let email = info[infoData].email;
+//     let message = info[infoData].message;
+//     console.log(name, email, message);
+
+//     let infosResults = document.querySelectorAll(".infosResults")
+
+//     infosResults.innerHTML += `<div>
+//     <p><strong>Name: <strong/>${name}<br/>
+//     <a><strong>Email: <strong/>${email}</a> <br/>
+//     <a><strong>Message: <strong/>${message}</a> 
+//     </p>
+//     </div>`
+    
+//   }
+// }
+
+// retrieveInfo();
+
+
+// Send Email info function
+ function sendEmail(name, email, message){
+Email.send({
+  Host: "smtp.gmail.com",
+  Username: 'freshstatebev@gmail.com',
+  Password: "Fresh$tate99",
+  To: 'freshstatebev@gmail.com',
+  From: 'freshstatebev@gmail.com',
+
+  Subject: `${name} sent you a message`,
+  Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
+}) .then((message) => alert("Message Sent Successfully"))
+
+ }
+
+
+ function getRandomCocktail(){
+  fetch('www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+ }
