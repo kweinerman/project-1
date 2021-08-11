3f0d68291f1a20fd2a3c1698fbe1b5c2470f0aba
kweinerman <85956994+kweinerman@users.noreply.github.com>
2021-08-10 21:35:02 -0600

    Updated html, css and JS

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-generateDots();
+
 //Init map function to display map plus markers
 function initMap() {
   //map options
@@ -138,85 +138,52 @@ function initMap() {
 
 //  }
 
-// function getRandomCocktail() {
-//   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-//     .then(function (response) {
-//       if (response.status !== 200) {
-//         console.log(
-//           "Looks like there was a problem. Status Code: " + response.status
-//         );
-//         return;
-//       }
 
-//       // Examine the text in the response
-//       response.json().then(function (data) {
-//         console.log(data);
-//       });
-//     })
-//     .catch(function (err) {
-//       console.log("Fetch Error :-S", err);
-//     });
-// }
+// Cocktail Api Starts Here
 
-// function getRandomCocktail() {
-//   fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-//     .then(function (response) {
-//       if (response.status !== 200) {
-//         console.log(
-//           "Looks like there was a problem. Status Code: " + response.status
-//         );
-//         return;
-//       }
+// API https://www.thecocktaildb.com/api/json/v1/1/random.php
 
-//       // Examine the text in the response
-//       response.json().then(function (data) {
-//         displayRandomCocktail(data);
-//       });
-//     })
-//     .catch(function (err) {
-//       console.log("Fetch Error :-S", err);
-//     });
-// }
+console.log('Connected!');
+// $("#recipe").hide();
+$("footer").hide();
+$("#recipe-card").hide();
 
-// getRandomCocktail();
+$(document).ready(function() {
+   
 
-// function displayRandomCocktail(cocktail) {
-//   console.log(cocktail.drinks[0]);
-//   let drinkSection = document.getElementById("random-cocktail");
+// generate random cocktail on button click
+    $('#generate').click(function() {  
 
-//   let drinkName = document.createElement("h2");
+        axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+        .then(function(response) {
+            console.log(response.data.drinks[0]);            
 
-//   drinkName.innerHTML = cocktail.drinks[0].strDrink;
+            $("#drink-name").html('<h2>' + response.data.drinks[0]["strDrink"] + '</h2>');   
+            $("#image").html('<img src=' + response.data.drinks[0]["strDrinkThumb"] + '></img>');
+            $("#category").html("<p id='category-info'><strong>Category:</strong> " + response.data.drinks[0]["strCategory"] + '</p>');
+            $("#type").html("<p><strong>Type:</strong> " + response.data.drinks[0]["strAlcoholic"] + '</p>');
+          
+            $("#recipe").show();
+            
+            $("#instructions").html('<p>' + response.data.drinks[0]["strInstructions"] +'</p>');
+            $("#recipe-card").show();
+            $("footer").show();
+            
 
-//   drinkSection.appendChild(drinkName);
+        });
+    });
 
-//   let img = document.createElement("img");
-//   img.src = cocktail.drinks[0].strDrinkThumb;
 
-//   drinkSection.appendChild(img);
+});
 
-  // for (let i = 1; i < 16; i++) {
-  //   if (
-  //     cocktail.drinks[0][`strIngredient${i}`] == null ||
-  //     cocktail.drinks[0][`strIngredient${i}`] == ""
-  //   ) {
-  //     break;
-  //   }
+//Reset scroll top
 
-  //   let ingredient = document.createElement("ons-list-item");
-  //   ingredient.innerHTML =
-  //     cocktail.drinks[0][`strIngredient${i}`] +
-  //     ":" +
-  //     cocktail.drinks[0][`strMeasure${i}`];
+history.scrollRestoration = "manual";
 
-  //   drinkSection.appendChild(ingredient);
-  // }
+$(window).on('beforeunload', function(){
+      $(window).scrollTop(0);
+});
 
-//   let card = document.createElement("ons-card");
-//   card.innerHTML = cocktail.drinks[0].strInstructions;
-
-//   drinkSection.appendChild(card);
-// }
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -249,74 +216,18 @@ function showSlides(n) {
   dots[slideIndex - 1].className += " active";
 }
 
-function generateDots() {
-  var slides = document.getElementsByClassName("mySlides");
-  console.log(slides);
-  const dotsContainer = document.getElementById("getDots");
-  for (let i = 0; i < slides.length; i++) {
-    let dotEl = document.createElement("span");
-    console.log(dotEl);
-    dotEl.setAttribute("class", "dot");
-    dotEl.addEventListener("onclick", currentSlide(i + 1));
-    dotsContainer.append(dotEl);
-  }
-}
+// function generateDots() {
+//   var slides = document.getElementsByClassName("mySlides");
+//   console.log(slides);
+//   const dotsContainer = document.getElementById("getDots");
+//   for (let i = 0; i < slides.length; i++) {
+//     let dotEl = document.createElement("span");
+//     console.log(dotEl);
+//     dotEl.setAttribute("class", "dot");
+//     dotEl.addEventListener("onclick", currentSlide(i + 1));
+//     dotsContainer.append(dotEl);
+//   }
+// }
 
 // Cocktail Api Starts Here
 
-// API https://www.thecocktaildb.com/api/json/v1/1/random.php
-
-console.log('Connected!');
-// $("#recipe").hide();
-$("footer").hide();
-$("#recipe-card").hide();
-
-$(document).ready(function() {
-   
-
-// generate random cocktail on button click
-    $('#generate').click(function() {  
-
-        axios.get('https://www.thecocktaildb.com/api/json/v1/1/random.php')
-        .then(function(response) {
-            console.log(response.data.drinks[0]);            
-
-            $("#drink-name").html('<h2>' + response.data.drinks[0]["strDrink"] + '</h2>');   
-            $("#image").html('<img src=' + response.data.drinks[0]["strDrinkThumb"] + '></img>');
-            $("#category").html("<p id='category-info'><strong>Category:</strong> " + response.data.drinks[0]["strCategory"] + '</p>');
-            $("#type").html("<p><strong>Type:</strong> " + response.data.drinks[0]["strAlcoholic"] + '</p>');
-          
-            $("#recipe").show();
-
-            // ingredients         
-            
-            // figure out a way to handle null or "" lines -- probably need a for-loop
-          
-
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure1"] + " " + response.data.drinks[0]["strIngredient1"] + '</li>');
-            
-          
-        
-            
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure1"] + " " + response.data.drinks[0]["strIngredient1"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure2"] + " " + response.data.drinks[0]["strIngredient2"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure3"] + " " + response.data.drinks[0]["strIngredient3"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure4"] + " " + response.data.drinks[0]["strIngredient4"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure5"] + " " + response.data.drinks[0]["strIngredient5"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure6"] + " " + response.data.drinks[0]["strIngredient6"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure7"] + " " + response.data.drinks[0]["strIngredient7"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure8"] + " " + response.data.drinks[0]["strIngredient8"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure9"] + " " + response.data.drinks[0]["strIngredient9"] + '</li>');
-            // $("#ingredients-list").append('<li>' + response.data.drinks[0]["strMeasure10"] + " " + response.data.drinks[0]["strIngredient10"] + '</li>');
-
-            
-            $("#instructions").html('<p>' + response.data.drinks[0]["strInstructions"] +'</p>');
-            $("#recipe-card").show();
-            $("footer").show();
-            
-
-        });
-    });
-
-
-});
