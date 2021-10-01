f2a1bd62d04069855e97da4a495ecc5f6c3e5e17
John Lundy <johnlundy94@gmail.com>
2021-09-30 18:30:39 -0600

    Added models and updated server

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
-var express = require('express');
-var path = require('path');
-var hbs = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
+const session = require("express-session");
 
-var routes = require('./routes/index');
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
-var app = express();
+const app = express();
+const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
 
+const sess = {
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
+app.use(session(sess));
 // view engine setup
 
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
