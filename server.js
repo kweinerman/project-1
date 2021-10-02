83263a9b24ea1b6fde5819f0754d44859dd6029f
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2021-10-02 11:20:54 -0600

    Homepage is rendering

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const bodyParse = require("body-parse");
+// const morgan = require("morgan");
+// const bodyParse = require("body-parse");
 
 const sequelize = require("./config/connection");
 const bodyParser = require("body-parser");
@@ -36,34 +36,20 @@ app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extendeed: true }));
-app.use(cookieParser());
+// app.use(morgan("dev"));
+// app.use(bodyParser.urlencoded({ extendeed: true }));
+// app.use(cookieParser());
 app.use(session(sess));
 
-app.engine(
-  "hbs",
-  hbs({
-    extname: "hbs",
-    defaultLayout: "layout",
-    layoutsDir: __dirname + "/views/layout",
-  })
-);
-app.set("view engine", "hbs");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-app.use((req, res, next) => {
-  if (req.cookies.user_sid && !req.session.user) {
-    res.clearCookie("user_sid");
-  }
-  next();
-});
-
-var hbsContent = {
-  userName: "",
-  loggedin: faslse,
-  title: "You are not logged in!",
-  body: "Drink More Kombucha",
-};
+// var hbsContent = {
+//   userName: "",
+//   loggedin: faslse,
+//   title: "You are not logged in!",
+//   body: "Drink More Kombucha",
+// };
 
 var sessionChecker = (req, res, next) => {
   if (req.session.user && req.cookies.user_sid) {
