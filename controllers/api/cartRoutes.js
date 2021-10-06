66d2dd4d0b343e8912712d70fc2f402576004b0a
johnlundy94 <79663204+johnlundy94@users.noreply.github.com>
2021-10-06 17:56:14 -0600

    Working on combining shopping cart

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -1,52 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var fs = require('fs');
-
-var Cart = require('../models/cart');
-var products = JSON.parse(fs.readFileSync('./data/products.json', 'utf8'));
-
-router.get('/', function (req, res, next) {
-  res.render('index', 
-  { 
-    title: 'Fresh State Kombucha',
-    products: products
-  }
-  );
-});
-
-router.get('/add/:id', function(req, res, next) {
-  var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {});
-  var product = products.filter(function(item) {
-    return item.id == productId;
-  });
-  cart.add(product[0], productId);
+router.put("/", function (req, res, next) {
+  var cart = req.session.cart ? req.session.cart : [];
+  cart.push(req.body);
   req.session.cart = cart;
-  res.redirect('/');
 });
 
-router.get('/cart', function(req, res, next) {
+router.get("/cart", function (req, res, next) {
   if (!req.session.cart) {
-    return res.render('cart', {
-      products: null
+    return res.render("cart", {
+      products: null,
     });
   }
-  var cart = new Cart(req.session.cart);
-  res.render('cart', {
-    title: 'Fresh State Kombucha',
-    products: cart.getItems(),
-    totalPrice: cart.totalPrice
+  var cart = req.session.cart;
+  res.render("cart", {
+    title: "Fresh State Kombucha",
+    products: cart,
+    totalPrice: cart.reduce((previousValue, currentValue) => {
+      return previousValue.price + currentValue.price;
+    }),
   });
 });
 
-router.get('/remove/:id', function(req, res, next) {
+router.delete("/:id", function (req, res, next) {
   var productId = req.params.id;
-  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  var cart = req.session.cart ? req.session.cart : [];
 
-  cart.remove(productId);
-  req.session.cart = cart;
-  res.redirect('/cart');
+  var newCart = cart.filter((product) => product.id !== productId);
+  req.session.cart = newCart;
+  res.redirect("/cart");
 });
 
 module.exports = router;
